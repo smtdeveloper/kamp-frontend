d077355f236dd59316f6af0509df31a354eb1716
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-03-11 00:57:41 +0300

    10 mart

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { ProductResponseModel } from '../models/productResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = 'http://localhost:61882';
+
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<ProductResponseModel>{
+    
+    return this.httpClient.get<ProductResponseModel>(this.apiUrl)
+   
+    
+ }
+
+
+}
+ 
